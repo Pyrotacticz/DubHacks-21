933a601a6d02a3746ba61e0782f6f60879c52ba2
jatruong <jatruong@uw.edu>
2021-10-23 23:54:07 -0700

    added secondary site, dbconnection.

--- app.js
+++ app.js
@@ -28,6 +28,54 @@ const HOST_NUMBER = 8080;
   return db;
 }
 
-app.use(express.static('public'));
+app.use("/", express.static('public'));
 const PORT = process.env.PORT || HOST_NUMBER;
-app.listen(PORT);
+app.listen(PORT, () => { console.log('FastCare running on ' + PORT + '...')});
+
+// @req - represents any data with the request
+// @res - used for sending a response back to the browser
+app.get("/test-sites", (req, res) => {
+  res.send("TODO: List Test sites or Clinical centers");
+  var rows = readTable("wi_clinics");
+});
+
+// catch 404, forward to error handler
+app.use(function(req, res, next) {
+  next(createError(CLIENT_ERROR));
+});
+
+
+// read from the db
+function readTable (table) {
+  let sql = `SELECT * FROM ${table}`;
+  let data = {};
+  var db = new sqlite3.Database('medical_centers.db', (err) => {
+    if (err) return console.error(err.message);
+    console.log('Connected to sqlite3 database.')
+  });
+
+  // return all results of query
+  db.all(sql, [], (err, rows) => {
+    if (err) throw(err);
+    rows.forEach((row) =>  {
+      // TODO: LIST IN FRONTEND
+      console.log(`${row.name} - Address: ${row.address} - Phone number: ${row.phone} - Hours: ${row.hours}`);
+    });
+    return rows;
+  });
+
+  db.close();
+}
+
+// TODO filter table
+
+
+// required for updateRows
+var schema = {
+  "wi_clinics": [
+    "name", "address", "phone", "hour"],
+  "test_sites": [
+    "name", "address", "phone", "hour"]
+}
+
+//TODO: createRow, updateRow, deleteRow (API For CLINICS AND HOSPITALS)
